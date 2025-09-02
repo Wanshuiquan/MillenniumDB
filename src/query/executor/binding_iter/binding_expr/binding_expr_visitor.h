93455e2c253a24075bfde2be56f3bdd7b771f947
Vicente Calisto <37163344+VicenteVicente@users.noreply.github.com>
2025-09-02 13:36:52 -0300

    Feat/MQLEditDistance (#67)
    * feat: edit distance
    
    * feat: normalize func mql
    
    ---------
    
    Co-authored-by: Carlos Rojas <cirojas6@uc.cl>

--- src/query/executor/binding_iter/binding_expr/binding_expr_visitor.h
+++ src/query/executor/binding_iter/binding_expr/binding_expr_visitor.h
@@ -26,6 +26,8 @@ class BindingExprCosineSimilarity;
 class BindingExprCosineDistance;
 class BindingExprManhattanDistance;
 class BindingExprEuclideanDistance;
+class BindingExprEditDistance;
+class BindingExprNormalize;
 } // namespace MQL
 
 namespace SPARQL {
@@ -188,6 +190,8 @@ public:
     virtual void visit(MQL::BindingExprCosineDistance&)    { throw LogicException("visit MQL::BindingExprCosineDistance not implemented"); }
     virtual void visit(MQL::BindingExprManhattanDistance&) { throw LogicException("visit MQL::BindingExprManhattanDistance not implemented"); }
     virtual void visit(MQL::BindingExprEuclideanDistance&) { throw LogicException("visit MQL::BindingExprEuclideanDistance not implemented"); }
+    virtual void visit(MQL::BindingExprEditDistance&)      { throw LogicException("visit MQL::BindingExprEditDistance not implemented"); }
+    virtual void visit(MQL::BindingExprNormalize&)         { throw LogicException("visit MQL::BindingExprNormalize not implemented"); }
 
     virtual void visit(SPARQL::BindingExprAbs&)            { throw LogicException("visit SPARQL::BindingExprAbs not implemented"); }
     virtual void visit(SPARQL::BindingExprAddition&)       { throw LogicException("visit SPARQL::BindingExprAddition not implemented"); }
