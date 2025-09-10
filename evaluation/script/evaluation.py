a4848abbf11d7f5959dfe6bfac98d47d1e1f63a8
Wanshuiquan <liheyang45@gmail.com>
2025-09-10 22:16:16 +0200

    fix typo

--- evaluation/script/evaluation.py
+++ evaluation/script/evaluation.py
@@ -1,9 +1,9 @@
-from evaluating import paradise_naive
+from evaluating import pokec_naive
 from evaluating import option as op
 from evaluating import util as util
 import threading
 import subprocess
 
 if __name__ == "__main__":
-        paradise_naive.icij_graph_query()
+        pokec_naive.pokec_graph_query()
         print("finish testing")
